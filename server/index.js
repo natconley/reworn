94b36bebb864c2884aa5234597407e1b4e9010bc
natconley <natalieannaconley@gmail.com>
2026-08-25 11:43:56 +0200

    Set up express routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const app = express();
+const productsRouter = require('./routes/products');
 
+const app = express();
 const PORT = 3000;
+
+app.use('/api/products', productsRouter);
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`);
---
+++ server/routes/products.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { getAllProducts } = require('../repositories/productRepository');
+
+router.get('/', async (req, res) => {
+    try {
+      const products = await getAllProducts();
+res.json(products);  
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch products' });
+    }
+});
+
+module.exports = router;
