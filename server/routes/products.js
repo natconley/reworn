0b8d7dcfb359e0c40e56c9638a478cae931336b4
natconley <natalieannaconley@gmail.com>
2026-08-26 11:10:58 +0200

    Add route getProductBySlug

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts } = require('../repositories/productRepository');
+const { getAllProducts, getProductBySlug } = require('../repositories/productRepository');
 
 router.get('/', async (req, res) => {
     try {
@@ -9,6 +9,21 @@ res.json(products);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Failed to fetch products' });
+    }
+});
+
+router.get('/:slug', async (req, res) => {
+    try {
+        const slug = req.params.slug;
+        const product = await getProductBySlug(slug);
+         if (!product) {
+            res.status(404).json({ error: 'Product could not be found' });
+        } else {
+        res.json(product);
+       }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch product' });
     }
 });
 
