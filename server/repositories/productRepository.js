7262a4266cc15f201234fd1bec0177cf60915381
natconley <natalieannaconley@gmail.com>
2026-08-26 10:55:31 +0200

    Add getProductBySlug

--- server/repositories/productRepository.js
+++ server/repositories/productRepository.js
@@ -1,5 +1,6 @@
 const pool = require('../db');
 
+// Get All Products
 async function getAllProducts({ onlyPublished = false } = {}) {
 let query = `
 SELECT p.*, c.name AS category_name, e.name AS era_name, col.name AS color_name, cond.name AS condition_name 
@@ -19,4 +20,19 @@ return result.rows;
 
 }
 
-module.exports = { getAllProducts };
+// Get Product by Slug
+async function getProductBySlug(slug) {
+   let query = `
+SELECT p.*, c.name AS category_name, e.name AS era_name, col.name AS color_name, cond.name AS condition_name 
+FROM products p 
+JOIN categories c ON p.category_id = c.id 
+JOIN eras e ON p.era_id = e.id 
+JOIN colors col ON p.color_id = col.id 
+JOIN conditions cond ON p.condition_id = cond.id 
+WHERE p.slug = $1 AND published_date <= NOW();
+`;
+
+const result = await pool.query(query, [slug]);
+return result.rows[0];
+}
+module.exports = { getAllProducts, getProductBySlug };
--- server/routes/products.js
+++ server/routes/products.js
@@ -12,4 +12,5 @@ res.json(products);
     }
 });
 
+
 module.exports = router;
