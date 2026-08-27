89de202b3a09f7613d18e43abd01afbf68573e16
natconley <natalieannaconley@gmail.com>
2026-08-27 11:06:39 +0200

    Add search and delete product in repository and routes

--- server/repositories/productRepository.js
+++ server/repositories/productRepository.js
@@ -1,15 +1,18 @@
 const pool = require('../db');
 
+// DRY
+const baseQuery = `
+  SELECT p.*, c.name AS category_name, e.name AS era_name, col.name AS color_name, cond.name AS condition_name 
+  FROM products p 
+  JOIN categories c ON p.category_id = c.id
+  JOIN eras e ON p.era_id = e.id
+  JOIN colors col ON p.color_id = col.id
+  JOIN conditions cond ON p.condition_id = cond.id
+`;
+
 // Get All Products
 async function getAllProducts({ onlyPublished = false } = {}) {
-let query = `
-SELECT p.*, c.name AS category_name, e.name AS era_name, col.name AS color_name, cond.name AS condition_name 
-FROM products p 
-JOIN categories c ON p.category_id = c.id
-JOIN eras e ON p.era_id = e.id
-JOIN colors col ON p.color_id = col.id
-JOIN conditions cond ON p.condition_id = cond.id
-`;
+let query = baseQuery;
 
 if (onlyPublished) {
     query += ' WHERE published_date <= NOW()';
@@ -22,17 +25,26 @@ return result.rows;
 
 // Get Product by Slug
 async function getProductBySlug(slug) {
-   let query = `
-SELECT p.*, c.name AS category_name, e.name AS era_name, col.name AS color_name, cond.name AS condition_name 
-FROM products p 
-JOIN categories c ON p.category_id = c.id 
-JOIN eras e ON p.era_id = e.id 
-JOIN colors col ON p.color_id = col.id 
-JOIN conditions cond ON p.condition_id = cond.id 
-WHERE p.slug = $1 AND published_date <= NOW();
-`;
+   let query = baseQuery + ' WHERE p.slug = $1 AND published_date <= NOW();';
 
 const result = await pool.query(query, [slug]);
 return result.rows[0];
 }
-module.exports = { getAllProducts, getProductBySlug };
+
+// search products
+async function searchProducts(searchTerm) {
+    const query = baseQuery + ' WHERE p.name ILIKE $1 AND published_date <= NOW()';
+    const pattern = `%${searchTerm}%`;
+
+    const result = await pool.query(query, [pattern]);
+    return result.rows;
+}
+
+// delete product
+async function deleteProduct(id) {
+    const query = ' DELETE FROM products WHERE id = $1';
+    const result = await pool.query(query, [id]);
+    return result.rowCount;
+}
+
+module.exports = { getAllProducts, getProductBySlug, searchProducts, deleteProduct };
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts, getProductBySlug } = require('../repositories/productRepository');
+const { getAllProducts, getProductBySlug, searchProducts, deleteProduct } = require('../repositories/productRepository');
 
+// get all products
 router.get('/', async (req, res) => {
     try {
       const products = await getAllProducts();
@@ -12,6 +13,19 @@ res.json(products);
     }
 });
 
+// search product by name
+router.get('/search', async (req, res) => {
+    try {
+        const searchTerm = req.query.search;
+        const products = await searchProducts(searchTerm);
+        res.json(products);
+        } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch products' });
+    }
+});
+
+// get product by slug
 router.get('/:slug', async (req, res) => {
     try {
         const slug = req.params.slug;
@@ -26,6 +40,24 @@ router.get('/:slug', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch product' });
     }
 });
+
+//delete product
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedCount = await deleteProduct(id);
+        if (deletedCount === 0) {
+            res.status(404).json({ error: 'No product was found to delete' });
+        } else {
+            res.status(200).json({ message: 'Product has been deleted' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch product' });
+    }
+})
+
+
 
 
 module.exports = router;
