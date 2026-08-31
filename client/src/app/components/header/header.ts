7a5958b6eb66cdd88d3cf1f9651c197d0b1aa59d
natconley <natalieannaconley@gmail.com>
2026-08-31 11:58:39 +0200

    add header component, no styling

--- client/src/app/components/header/header.ts
+++ client/src/app/components/header/header.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  private router = inject(Router);
+
+  searchTerm = '';
+  onSearch(): void {
+    this.router.navigate(['/search'], {queryParams: { search: this.searchTerm } });
+  }
+}
