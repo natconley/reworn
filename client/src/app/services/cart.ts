1c5d17c4023f9133bda0549e04ebc831108611ab
natconley <natalieannaconley@gmail.com>
2026-09-09 14:45:25 +0200

    add cartService addToCart, removeFromCart and updateQuantity

---
+++ client/src/app/services/cart.ts
@@ -0,0 +1,49 @@
+import { Injectable, signal, computed } from '@angular/core';
+import { Product } from '../models/product';
+
+interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  private items = signal<CartItem[]>([]);
+
+  addToCart(product: Product): void {
+    this.items.update(currentItems => {
+      const existingItem = currentItems.find(item => item.product.id === product.id);
+
+      if (existingItem) {
+        return currentItems.map(item => 
+          item.product.id === product.id
+          ? { ...item, quantity: item.quantity + 1}
+          : item
+        );
+      } else {
+        return [...currentItems, { product, quantity: 1 }];
+      }
+    });
+  }
+
+  removeFromCart(product: Product): void {
+    this.items.update(currentItems => 
+      currentItems.filter(item => item.product.id !== product.id)
+      );
+    }
+
+  updateQuantity(productId: number, delta: number): void {
+    this.items.update(currentItems => {
+      const updated = currentItems.map(item =>
+        item.product.id === productId
+        ? { ...item, quantity: item.quantity + delta }
+        : item
+      );
+      return updated.filter(item => item.quantity > 0);
+    });  
+    } 
+}
+
+  
