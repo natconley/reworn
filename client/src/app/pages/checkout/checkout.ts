5b3ac9137b1cd49b56d8a6a7d54970e66766fa69
natconley <natalieannaconley@gmail.com>
2026-09-12 20:36:02 +0200

    Add checkout with styling

--- client/src/app/pages/checkout/checkout.ts
+++ client/src/app/pages/checkout/checkout.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart';
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
