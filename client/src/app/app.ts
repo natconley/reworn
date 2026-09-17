e05b2f51b3bb9972a164f8aaf40082414060ddda
natconley <natalieannaconley@gmail.com>
2026-09-17 15:04:22 +0200

    Add styling details, add separate admin header

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,14 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, computed, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs';
 import { Header } from './components/header/header';
+import { AdminHeader } from './components/admin-header/admin-header';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, AdminHeader, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('client');
+  private router = inject(Router);
+
+  // for new header component in admin panel
+  currentUrl = toSignal(
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => (event as NavigationEnd).url)
+    ),
+    { initialValue: this.router.url }
+  );
+
+  isAdminRoute = computed(() => {
+    const result = this.currentUrl().startsWith('/admin');
+  console.log('isAdminRoute:', result, 'currentUrl:', this.currentUrl());
+    return result
+});
 }
---
+++ client/src/app/components/admin-header/admin-header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-header',
+  imports: [RouterLink],
+  templateUrl: './admin-header.html',
+  styleUrl: './admin-header.css',
+})
+export class AdminHeader {}
